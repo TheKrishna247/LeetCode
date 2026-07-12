dd93e34bb268d0617444e07c278df0665b88b867
Krishna Tomar <thekrishna7tomar@gmail.com>
2026-07-12 11:10:43 +0530

    Time: 1 ms (97.65%), Space: 42.7 MB (23.29%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,14 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+        if (x==0) return 0 ;
+        int low =1 , high = x;
+        while(low<=high){
+            int mid = low + (high -low )/2;
+            if(mid == x/mid ) return mid;
+            else if(mid > x/mid) high = mid -1;
+            else low = mid + 1;
+        }
+        return high;
+        
     }
 }
