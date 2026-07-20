67034f2f5d08c638766611cb328992879ce6c086
Krishna Tomar <thekrishna7tomar@gmail.com>
2026-07-21 00:28:52 +0530

    Time: 0 ms (100%), Space: 52.9 MB (58.49%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,26 +1,19 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         int n = nums.length;
-        if (n == 1) return nums[0];
-        else if (nums[0] != nums[1]) return nums[0];
-        else if (nums[n-2] != nums[n-1]) return nums[n-1];
-        int low = 0 , high = n-1;
-
-        while(low <= high){
-            int mid = low + (high - low)/2;
-            if (nums[mid-1] != nums[mid] && nums[mid] != nums[mid+1])
+        if(n == 1) return nums[0];
+        if(nums[0]!= nums[1]) return nums[0];
+        if(nums[n-2]!= nums[n-1]) return nums[n-1];
+        
+        int low = 1 , high = n-2;
+        while(low<=high){
+            int mid = (low + high)/2;
+            if (nums[mid]!=nums[mid-1] && nums[mid]!= nums[mid+1]) 
                 return nums[mid];
-            int fo = mid , so = mid;
-            if (nums[mid -1] == nums[mid])
-                fo = mid-1;
-            else // (nums[mid] == nums[mid+1])
-                so = mid+1;
-            
-            int leftcnt= fo-low , rightcnt = high - so;
-            if (leftcnt % 2 == 0) low = so + 1;
-            else // rightcnt %@ ==0
-                high = fo -1;
-        }   
+            else if( (mid%2 ==1 && nums[mid]== nums[mid-1]) || (mid %2 == 0 && nums[mid] == nums[mid+1]))
+                low = mid+1;
+            else high = mid-1;
+        }
         return -1;
     }
 }
