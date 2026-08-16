e99a5c475453e2288009ac05cddf20ae18475a73
Krishna Tomar <thekrishna7tomar@gmail.com>
2026-08-17 00:44:27 +0530

    Time: 7 ms (29.05%), Space: 114.9 MB (31.76%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int cnt0 = 0, cnt1 = 0 , cnt2=0;
+
+        for(int ele : stones){
+            if( ele % 3 == 0 )cnt0++;
+            else if( ele % 3 == 1 )cnt1++;
+            else cnt2++;
+        }
+        if(cnt0 % 2 == 0)   
+            return cnt1 >= 1 && cnt2 >= 1;
+        return Math.abs(cnt1-cnt2) >2;
+    }
+}
