968703d7093df5430d53e3899218c59dfe517efa
Krishna Tomar <thekrishna7tomar@gmail.com>
2026-08-31 12:11:34 +0530

    Time: 0 ms (100%), Space: 47.7 MB (78.45%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,6 +1,24 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        // return Math.pow(x,n);
+        if (n==0) return 1.0;
+        if (x == 0) return 0.0;
+        long N = n ;
+        if (n<0){
+            x = 1/x;
+            N = -N;
+        }
+        double res = 1.0;
+        double current = x ;
+
+        while(N>0){
+            if(N%2==1){
+                res = res*current;
+            }
+            current *=current;
+            N/=2;
+        }
+        return res;
     }
 }
 
